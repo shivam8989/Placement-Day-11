0f5f6a9180c25c2178511b7fb5dfa1ff0cc6eb19
SHIVAM SINGH <91870115+shivam8989@users.noreply.github.com>
2023-07-11 21:20:13 +0530

    Add files via upload
    Problems

---
+++ public class Main {.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(Strings [] args){
+        int x = 4;
+        int y = 4;
+        for(int i =1; i <=x; i++){
+            for(int j =1; j<=y; j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
